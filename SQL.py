028f162dcb4f9d15031fcb120393065960c37694
jeffreyevan <jeffrey_nave_evan@live.com>
2020-02-24 14:33:40 +0700

    initial commit

--- SQL.py
+++ SQL.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 import pandas as pd
+from pandas.io import sql
 
 data = pd.read_csv('csv.csv')
 
@@ -19,3 +20,19 @@ print('')
 res2 = pd.read_sql_query('SELECT dept, sum(salary) FROM data_table group by dept', engine)
 print('Result 2')
 print(res2)
+
+print('----------------------------')
+
+# Insert another row
+sql.execute('INSERT INTO data_table VALUES(?,?,?,?,?,?)', engine, params=[('id',9,'Ruby',711.20,'2015-03-27','IT')])
+
+# Read from the relational table
+res = pd.read_sql_query('SELECT ID,Dept,Name,Salary,start_date FROM data_table', engine)
+print(res)
+
+print('----------------------------')
+
+sql.execute('Delete from data_table where name = (?) ', engine,  params=[('Gary')])
+
+res = pd.read_sql_query('SELECT ID,Dept,Name,Salary,start_date FROM data_table', engine)
+print(res)
